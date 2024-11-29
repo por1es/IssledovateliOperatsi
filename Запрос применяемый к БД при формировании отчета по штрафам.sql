e7efc085aa3896d85d131698a81c015ece428194
por1es <96617497+por1es@users.noreply.github.com>
2024-11-29 22:02:37 +0300

    Update Запрос применяемый к БД при формировании отчета по штрафам.sql

--- Запрос применяемый к БД при формировании отчета по штрафам.sql
+++ Запрос применяемый к БД при формировании отчета по штрафам.sql
@@ -1,17 +1,19 @@
 SELECT 
-	ob."FRG_naimenovanieObekta" as "Объект", 
-	kp."FRG_naimenovanieKontrolnogoPokazatelya" as "Контрольный показатель",
-	ct."FRG_Name" as "Ответственный",
-	"FRG_summaShtrafa" as "Сумма штрафа"
+  ob."FRG_naimenovanieObekta" AS "Объект", 
+  kp."FRG_naimenovanieKontrolnogoPokazatelya" AS "Контрольный показатель",
+  ct."FRG_Name" AS "Ответственный",
+  CASE 
+    WHEN nar."FRG_shtraf" <= 100 THEN nar."FRG_shtraf" * nar."FRG_premiyaOtvetstvennogo" / 100
+    ELSE nar."FRG_shtraf"
+  END AS "Сумма штрафа",
+  nar."FRG_premiyaOtvetstvennogo",
+  nar."FRG_shtraf"
 FROM 
-	public."BoForeignInstancereestrNarushenii" nar
-	left join
-		public."BoForeignInstanceobektiKompanii" ob on ob."Id" = nar."FRG_obektProverki"
-	left join
-		public."BoForeignInstancespravochnikKontrolnikhPokazatelei" kp on kp."Id" = nar."FRG_kriteriiProverki"
-	left join
-		public."BoForeignInstancecontacts" ct on ct."Id" = nar."FRG_otvetstvennii"
-Where 
-	"FRG_srokUstraneniya" < "FRG_fakticheskiiSrokUstraneniya"
+  public."BoForeignInstancereestrNarushenii" nar
+  LEFT JOIN public."BoForeignInstanceobektiKompanii" ob ON ob."Id" = nar."FRG_obektProverki"
+  LEFT JOIN public."BoForeignInstancespravochnikKontrolnikhPokazatelei" kp ON kp."Id" = nar."FRG_kriteriiProverki"
+  LEFT JOIN public."BoForeignInstancecontacts" ct ON ct."Id" = nar."FRG_otvetstvennii"
+WHERE 
+  nar."FRG_srokUstraneniya" < (SELECT DATE(NOW()))
 	and
 	("FRG_dataRegistratsiiNarusheniya" >= @periodStart) and ("FRG_dataRegistratsiiNarusheniya" <= @periodEnd);
